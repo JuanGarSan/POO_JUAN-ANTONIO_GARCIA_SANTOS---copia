bc9d90110a54818a83448381c42845d42b741e95
JuanGarsan <ja.garcia.s1@edu.utc.mx>
2021-11-24 18:30:33 -0600

    01_Hola_Mundo

---
+++ 01_Hola_Mundo/HolaMundo.java
@@ -0,0 +1,28 @@
+//estos es un comentario
+
+/*
+esto es otro comentario los comentarios no van afertar 
+al codigo fuente de la clase
+*/
+
+class HolaMundo{
+//cuerpo del programa
+/*
+aqui es donde nosotros empezamos a codificar todas las funciones 
+que querramos para el programa
+
+metodo[acciones a programar]
+*/
+
+//toda clase debe de tener si solo si es 
+// la clase principal un metodo principal
+
+//el metodo principal un metodo principal 
+//a las acciones de todo el programa 
+
+        public static void main(String[] ars){
+        //esto es el cuerpo del metodo principal
+        System.out.print("Hola Mundo... :3");
+        }
+
+}
